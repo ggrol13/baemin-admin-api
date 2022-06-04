b0ccf129bed16234e33605fceb462cdd221e30ef
ggrol13 <ggrol13@gmail.com>
2022-06-04 10:18:51 +0900

    prettier semi false

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const options = {
   host: '0.0.0.0',
   port,
-};
+}
 
 app.listen(options, () => {
-  console.log('server is on port ' + port);
-});
+  console.log('server is on port ' + port)
+})
