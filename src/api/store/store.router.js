6beadaeedd041a5207e117edd50e045c639f2687
ggrol13 <ggrol13@gmail.com>
2022-07-02 11:17:45 +0900

    api success 개발

--- src/api/store/store.router.js
+++
@@ -1,8 +0,0 @@
-import { createStore, getStore } from './store.controller.js';
-import express from 'express';
-export const storeRouter = () => {
-  const router = express.Router();
-  router.post('/', createStore);
-  router.get('/:storeId', getStore);
-  return router;
-};
--- src/common/api-response.js
+++ src/common/api-response.js
@@ -1,3 +1,5 @@
+import { NO_CONTENT, OK } from './http-code.js';
+
 export const ApiError = (error, res) => {
   res.status(error.code).json({
     response: {
@@ -7,4 +9,18 @@ export const ApiError = (error, res) => {
   });
 };
 
-export const ApiSuccess = (callback, code) => {};
+export const ApiSuccess = async (callback, res) => {
+  let success = OK;
+  const data = await callback();
+  if (!data || data.length === 0) {
+    success = NO_CONTENT;
+  }
+
+  res.status(success.code).json({
+    response: {
+      message: success.message,
+      status: true,
+      body: data,
+    },
+  });
+};
