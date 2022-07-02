7ca6f8a61f241cd0cd5f95cf0a98f3484f33fe43
ggrol13 <ggrol13@gmail.com>
2022-07-02 11:24:51 +0900

    delete unused router

--- src/middleware/router.main.js
+++ src/middleware/router.main.js
@@ -1,5 +1,1 @@
-import { storeRouter } from '../api/store/store.router.js';
-
-export const routerV1 = (app) => {
-  app.use('/api/v1/store', storeRouter());
-};
+export const routerV1 = (app) => {};
