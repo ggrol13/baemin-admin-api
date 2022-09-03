76e2568008c6cd25fa94734f90772c2945635745
ggrol13 <ggrol13@gmail.com>
2022-09-03 10:59:14 +0900

    Changes by minchang-gyeong

--- src/app.js
+++ src/app.js
@@ -8,17 +8,20 @@ import { connectMongo } from "./middleware/mongoose.js";
 import { routerV1 } from "./middleware/router.main.js";
 
 (async () => {
-  dotenv.config();
+  await dotenv.config();
   const port = process.env.PORT || 3000;
   await connectMongo().catch();
 
   app.use(morgan("dev"));
+  app.use((req, res, next) => {
+    req.multer = {};
+    next();
+  });
   app.use(methodOverride());
   app.use(bodyParser.json());
   // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
   // app.use(bodyParser.urlencoded({ extended: false }));
   routerV1(app);
-
   app.listen(3000);
 
   const options = {
